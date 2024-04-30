b55ed185c5695cb5b77521bdac7fd32f9f0e10b0
Marc <mail@lpcvoid.com>
2024-04-30 21:43:36 +0200

    updated sway config; updated sway style; updated alias

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,8 @@
 set -gx TERM xterm-256color
 set PATH ~/.local/bin $PATH
 set PATH ~/.cargo/bin $PATH
+set PATH ~/tools $PATH
 set -gx _JAVA_AWT_WM_NONREPARENTING 1
-fish_ssh_agent
+#fish_ssh_agent
+alias cpv="rsync --progress -ah"
+alias mvv="rsync --progress -ah --remove-sent-files"
