6111e6512e41d27a6be4dc54166cfeae778ca30d
Marc <mail@lpcvoid.com>
2024-05-20 11:34:00 +0200

    add nvim to gather/deply

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,6 @@ cp -r ./sway/* $HOME/.config/sway
 cp -r ./waybar/* $HOME/.config/waybar
 cp -r ./mako/* $HOME/.config/mako
 cp -r ./fish/* $HOME/.config/fish
+cp -r ./nvim/* $HOME/.config/nvim
 
 sudo cp ./environment /etc/environment
--- gather.sh
+++ gather.sh
@@ -4,6 +4,7 @@ cp -r $HOME/.config/sway .
 cp -r $HOME/.config/waybar .
 cp -r $HOME/.config/mako .
 cp -r $HOME/.config/fish .
+cp -r $HOME/.config/nvim .
 
 #environment
 cp /etc/environment .
